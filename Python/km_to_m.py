ac8af6d3ec26cf8802fea9951f23582d987c8b21
avinishrao <avinishrao@gmail.com>
2022-10-14 11:26:38 +0530

    Python Kilometers to meters

---
+++ Python/km_to_m.py
@@ -0,0 +1,7 @@
+# Program to convert Kilometers to Meters
+# Value in kilometer
+kilometers = float(input("Enter the value in kilometers: "))
+
+# calculate meters
+meters = kilometers * 1000
+print('%0.2f kilometers = %0.2f meters' %(kilometers,meters))
